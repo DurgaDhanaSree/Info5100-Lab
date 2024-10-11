354bc134c530e6fc99001f16d51c2740c4f1243f
DurgaDhanaSree <chilukuri.du@northeastern.edu>
2024-10-10 21:40:10 -0400

    Completed the Assignment Succesfully

---
+++ LAB-3/LAB3NAVIGATION/MainJFrame3/src/Model/PatientClass.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author 91903
+ */
+public class PatientClass {
+    private String name;
+    private String last_name;
+    private String Gender;
+    private String patientType;
+    private ImageIcon profilePicture;
+    private int age;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getPatientType() {
+        return patientType;
+    }
+
+    public void setPatientType(String patientType) {
+        this.patientType = patientType;
+    }
+
+    public ImageIcon getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(ImageIcon profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }    }
+
+   
